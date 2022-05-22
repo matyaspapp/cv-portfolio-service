da8724ba230e147978077249598b6c1a7865a528
papp.matyas <papp.matyas@drescher.hu>
2022-05-22 21:39:44 +0200

    update database services type annotation

--- app/database/service.py
+++ app/database/service.py
@@ -1,6 +1,6 @@
 from bson import ObjectId
 from pymongo import MongoClient
-from typing import TypeVar
+from typing import Any, TypeVar
 
 
 T = TypeVar('T')
@@ -14,18 +14,18 @@ class CRUDService:
     def create(self, new_item: T) -> object:
         return self._conn.local[self._collecion].insert_one(new_item)
 
-    def get_by_id(self, id: str | ObjectId) -> T:
+    def get_by_id(self, id: str | ObjectId) -> object:
         id = ObjectId(id)
         return self._conn.local[self._collecion].find_one({'_id': id})
 
-    def get_all(self, **kwargs) -> list[T]:
+    def get_all(self, **kwargs) -> list[object]:
         if 'owner_id' in kwargs:
             cursor = self._conn.local[self._collecion].find({'owner_id': kwargs['owner_id']})
         else:
             cursor = self._conn.local[self._collecion].find({})
         return [item for item in cursor]
 
-    def get_all_by_key(self, key, value):
+    def get_all_by_key(self, key: str, value: Any) -> list[object]:
         cursor = self._conn.local[self._collecion].find({key: value})
         return [item for item in cursor]
 
